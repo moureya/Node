fb4860fc8b588211bab4657a8d63cb02be46b3f1
Anthony <anthony@Mint-18.3-KDE>
2018-02-25 20:15:07 -0500

    Using Modules and taking input
    - Includes files up through Section 3: Lesson 15
    - Added .gitignore to leave out node_modules folder

--- Notes-Node/app.js
+++ Notes-Node/app.js
@@ -1,20 +1,24 @@
 console.log('Starting app.js');
 
 const fs = require('fs');
-const os = require('os');
+const _ = require('lodash');
+const yargs = require('yargs');
+
 const notes = require('./notes.js');
 
-let addition = notes.add(9, -2);
-console.log('Result:', addition);
+const argv = yargs.argv;
+let command = argv._[0];
+console.log('Command:', command);
+console.log('Yargs:', argv);
 
-// let user = os.userInfo();
-//
-// fs.appendFile(
-// 	'greetings.txt',
-// 	`Hello ${user.username}! You are ${notes.age}.`,
-// 	function(err) {
-// 		if (err) {
-// 			console.log('Unable to write to file');
-// 		}
-// 	}
-// );
+if (command === 'add') {
+	notes.addNote(argv.title, argv.body);
+} else if (command === 'list') {
+	notes.getAll();
+} else if (command === 'read') {
+	notes.getNote(argv.title);
+} else if (command === 'remove') {
+	notes.removeNote(argv.title);
+} else {
+	console.log('Command not recognized');
+}
