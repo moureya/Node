4b96d31c878f55d88b2acef25f70d6072b385e14
Anthony <anthony@Mint-18.3-KDE>
2018-02-28 20:55:00 -0500

    Weather-app first commit
    - Includes files up through Section 4: Lesson 30

---
+++ weather-app/app.js
@@ -0,0 +1,14 @@
+const request = require('request');
+
+request(
+	{
+		url:
+			'https://maps.googleapis.com/maps/api/geocode/json?address=280%20East%20Essex%20Lane%20Fort%20Wayne%20Indiana%2046825',
+		json: true
+	},
+	(error, response, body) => {
+		console.log(`Address: ${body.results[0].formatted_address}`);
+		console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+		console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+	}
+);
