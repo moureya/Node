be982188caff1ac59182f23f455077b3f6383a62
Anthony <moureya01@gmail.com>
2018-03-12 20:23:38 -0400

    Halfway through web server
    - Includes files up through Section 5: Lesson 45

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -1,17 +1,51 @@
 const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
 
 let app = express();
 
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+	let now = new Date().toString();
+	let log = `${now}: ${req.method} ${req.url}`;
+
+	console.log(log);
+	fs.appendFile('server.log', log + '\n', err => {
+		if (err) {
+			console.log('Unable to append to server.log');
+		}
+	});
+
+	next();
+});
+
+app.use((req, res, next) => {
+	res.render('maintanence.hbs');
+});
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+	return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', text => {
+	return text.toUpperCase();
+});
+
 app.get('/', (req, res) => {
-	//res.send('<h1>Hello Express!</h1>');
-	res.send({
-		name: 'Anthony',
-		likes: ['Computers', 'Cars']
+	res.render('home.hbs', {
+		pageTitle: 'Home Page',
+		welcomeMessage: 'Hello! Welcome to my fantastically wonderful webpage!'
 	});
 });
 
 app.get('/about', (req, res) => {
-	res.send('About Page');
+	res.render('about.hbs', {
+		pageTitle: 'About Page'
+	});
 });
 
 // /bad - send back json with errorMessage property
@@ -22,4 +56,6 @@ app.get('/bad', (req, res) => {
 	});
 });
 
-app.listen(3000);
+app.listen(3000, () => {
+	console.log('Server is up on port 3000');
+});
